bdeb8ff8d397f2720feee1b37db6d4d2b38a6126
Tharsanth <tharsanth@technoexponent.com>
2023-04-24 14:23:29 +0530

    url changed

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const server = app.listen(PORT, () =>
 const io = new Server(server, {
   pingTimeout : 600000,
   cors : {
-    origin : "https://devtest.zls.app"
+    origin : "https://dev.zls.app"
   }
 });
 const connectedClients = {};
