c8610ab53e79cbe02bf0e9eac43519c8ec0009bb
Tharsanth <tharsanth@technoexponent.com>
2023-04-24 14:23:52 +0530

    removed the text

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use("/api", postRoutes);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () =>
-  console.log(`Server Running on Port: http://localhost:${PORT}`)
+  console.log(`Server Running on Port: ${PORT}`)
 );
 
 const io = new Server(server, {
